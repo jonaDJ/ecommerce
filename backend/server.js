d0e1153bfae4a64489cdf850e8f904c7e6d74bf0
jonaDJ <jonathandarla255@gmail.com>
2023-11-04 19:22:27 -0700

    added controller,middleware- asyncHandler and errorMiddleware; updated routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 
@@ -10,19 +11,12 @@ connectDB();
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
-);
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server running on port ${port}`));
