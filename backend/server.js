23616cdb8444c9e2a56d2272e904591115056062
jonaDJ <jonathandarla255@gmail.com>
2023-11-09 20:41:27 -0800

    backend auth

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
-import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -11,11 +14,19 @@ connectDB();
 
 const app = express();
 
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  //set jwt as HTTP-Only cookie
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    sameSite: "strict", // Prevent CSRF attacks
+    maxAge: 30 * 24 * 60 * 60, //30 days
+  });
+};
+
+export default generateToken;
