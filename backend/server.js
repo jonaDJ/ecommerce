0e3b2119d634f9ee1591d5a080a336d6ad9bc103
jonaDJ <jonathandarla255@gmail.com>
2023-11-13 18:44:23 -0800

    check process 1 - added shipping, place order and payment scrrens - added 3 reducers in cart slice - add a private route and include them in index.js - added a CheckoutSteps component. - added order routes and controller

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -14,19 +15,14 @@ connectDB();
 
 const app = express();
 
-//body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Cookie parser middleware
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -29,9 +29,30 @@ const cartSlice = createSlice({
 
       return updateCart(state);
     },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+
+      return updateCart(state);
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+
+      return updateCart(state);
+    },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+  clearCartItems,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
