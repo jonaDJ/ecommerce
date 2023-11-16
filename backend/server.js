fda0d954018b7a5df0f5c1d305a89fd9cdfd172a
jonaDJ <jonathandarla255@gmail.com>
2023-11-15 20:24:14 -0800

    added build script in package.json

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,17 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build"))); //set static folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); //any routes which  are not api will be redirected to index.html
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
