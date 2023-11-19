d2193b386f14e0e7ced2965082b560ab2612bc4f
jonaDJ <jonathandarla255@gmail.com>
2023-11-18 20:34:15 -0800

    PayPal & Price Calculate Fixes

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -61,7 +61,7 @@ const OrderScreen = () => {
   const onApprove = (data, actions) => {
     return actions.order.capture().then(async (details) => {
       try {
-        await payOrder({ orderId, details });
+        await payOrder({ orderId, details }).unwrap();
         refetch();
         toast.success("Payment Succesful");
       } catch (err) {
